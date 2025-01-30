d74c952c0c551dc26b5d2d04e7f245fc1c108153
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2025-01-30 23:42:28 +0530

    nextconfig file added

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['assets.aceternity.com','aceternity.com'], // Add your image domain here
+  },
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
+  typescript:{
+    ignoreBuildErrors:true
   }
   /* config options here */
 };
